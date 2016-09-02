9588a3b050b02348ca7cfca0ed0f2f3bf888b11f
userxpgh <userxp25@qq.com>
2016-09-02 17:11:20 +0800

    xxxx

---
+++ test2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# import re
+#########
+# import ipaddress
+#
+# #net_ip = ipaddress.ip_network('172.17.41.212-220')
+# m = re.findall('(\d{1,3}\.\d{1,3}\.\d{1,3}\.)(.*)-(.*)', '172.17.41.21222d220')
+# if m:
+#     print(m)
+#     for i in range(int(m[0][1]), int(m[0][2])+1):
+#         print(m[0][0] + str(i))
+#
+# # print(net_ip)
+# for ip in net_ip:
+#     print(ip)
+
